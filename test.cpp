ba2d3e67e1aa7da1eb95f086107bbd2d1f5d78c7
dingsheng <dingsheng@pset.suntec.net>
2018-03-06 14:23:46 +0800

    delete \

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ int main()
 {
     TestHandler3* handler3 = new TestHandler3(0, HANDLER_NUM_3);
     TestHandler2* handler2 = new TestHandler2(handler3, HANDLER_NUM_2);
-    TestHandler1* handler1 = new TestHandler1(handler2, HANDLER_NUM_1);\
+    TestHandler1* handler1 = new TestHandler1(handler2, HANDLER_NUM_1);
     handler1->HandleMsg(HANDLER_NUM_1);
     handler1->HandleMsg(HANDLER_NUM_2);
     handler1->HandleMsg(HANDLER_NUM_3);
